d91e1213e5ff22f36be34100ce75ebb2d39b0a91
Mohammad Chand Alam <amuchand47@gmail.com>
2022-10-18 16:43:47 +0530

    Infinite Scroll Added

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
 
@@ -19,17 +20,16 @@ export class News extends Component {
   }
 
   capitalizeFirstletter=(string)=>{
-     
     return string.charAt(0).toUpperCase() + string.slice(1);
-
   }
 
   constructor(props) {
     super(props);
     this.state = {
       articles: [],
-      loading: true,
+      loading: false,
       page: 1,
+      totalResults: 0
     };
     document.title = `${this.capitalizeFirstletter(this.props.category)} - News Muzaffarpur`;
   }
@@ -37,7 +37,7 @@ export class News extends Component {
   async updateNews(){
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&pageSize=${
       this.props.pageSize
-    }&category=${this.props.category}&apiKey=399f6c86ba9f4e41bcdac7573691a64c&page=${
+    }&category=${this.props.category}&apiKey=cad510c1cfc448b4b5ca122567ac0125&page=${
       this.state.page
     }`;
     console.log(url);
@@ -48,7 +48,7 @@ export class News extends Component {
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
-      loading: false
+      loading: false,
     })
 
   }
@@ -57,34 +57,48 @@ export class News extends Component {
     this.updateNews();
   }
 
-  handleNextClick = async () => {
-    this.setState({page: this.state.page + 1});
-    this.updateNews();  
-  };
+  fetchMoreData = async() => {
+    
+    this.setState({page: this.state.page + 1 })
 
-  handlePrevClick = async () => {
-    this.setState({page: this.state.page - 1});
-    this.updateNews();
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&pageSize=${this.props.pageSize}&category=${this.props.category}&apiKey=cad510c1cfc448b4b5ca122567ac0125&page=${
+      this.state.page+1
+    }`;
+
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults,
+    })
+    
   };
 
   render() {
     return (
-      <div className="container my-3">
-        <h1 className="text-center"> Top News HeadLines from {this.capitalizeFirstletter(this.props.category)}</h1>
-        {this.state.loading && <Spinner />}
+      
+      < >
 
+        <h1 className="text-center"> Top News HeadLines from {this.capitalizeFirstletter(this.props.category)}</h1>
+        {/* {this.state.loading && <Spinner />} */}
+
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length!==this.state.totalResults}
+          loader={<Spinner/>}
+        >
+
+        <div className="container">
+
+        
         <div className="row">
-          {!this.state.loading && this.state.articles.map((element) => {
+          { this.state.articles.map((element) => {
             return (
               <div className="col-md-4" key={element.url}>
-                <NewsItem
-                  title={element.title.slice(0, 50)}
-                  description={
-                    element.description ? element.description.slice(0, 88) : ""
-                  }
-                  imageUrl={
-                    element.urlToImage
-                      ? element["urlToImage"]
+                <NewsItem title={element.title.slice(0, 50)}description={element.description ? element.description.slice(0, 88) : ""}
+                  imageUrl={element.urlToImage? element["urlToImage"]
                       : "https://c1.wallpaperflare.com/preview/608/687/767/the-gap-message-alarm-breaking-news-news.jpg"
                   }
                   newsUrl={element.url}
@@ -95,29 +109,12 @@ export class News extends Component {
               </div>
             );
           })}
+         </div>
+         </div>
+         </InfiniteScroll>  
 
-          <div className="container d-flex justify-content-between">
-            <button
-              disabled={this.state.page <= 1}
-              type="button"
-              className="btn btn-dark"
-              onClick={this.handlePrevClick}
-            >
-              &larr; Prevoius
-            </button>
-            <button
-              disabled={
-                Math.ceil(this.state.totalResults / 20) < this.state.page + 1
-              }
-              type="button"
-              className="btn btn-dark"
-              onClick={this.handleNextClick}
-            >
-              Next &rarr;
-            </button>
-          </div>
-        </div>
-      </div>
+      </>
+  
     );
   }
 }
